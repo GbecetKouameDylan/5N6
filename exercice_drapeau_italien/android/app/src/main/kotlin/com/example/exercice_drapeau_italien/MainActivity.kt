0022c1bc56afdf88f59f2beb88ba6a1e8009417f
D <2164425@cegepmontpetit.ca>
2024-06-06 09:38:22 -0400

    FCT:Drapeau Italie

---
+++ exercice_drapeau_italien/android/app/src/main/kotlin/com/example/exercice_drapeau_italien/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exercice_drapeau_italien
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ exercice_drapeau_italien/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+children: <Widget>[
+  Row(
+    children: [
+      Expanded(
+        flex: 1,
+        child: Container(
+          // Ca prend un container pour les bordures et les marges
+          color: Colors.green,
+          child: const Padding(
+            // Le padding se fait en encapsulant dans le widget Padding
+            padding: EdgeInsets.all(448.5),
+          ),
+        ),
+      ),
+      Expanded(
+        flex: 1,
+        // pas forcement necessaire parce que 1 c'est la valeur par defaut
+        child: Container(
+
+color: Colors.white,
+        ),
+      ),
+      Expanded(
+        flex: 1,
+        // pas forcement necessaire parce que 1 c'est la valeur par defaut
+        child: Container(
+            child: const Padding(
+              // Le padding se fait en encapsulant dans le widget Padding
+              padding: EdgeInsets.all(448.5),
+            ),
+            color: Colors.red,
+
+        )
+
+      )
+    ],
+  ),
+],
+
+      ),
+    );
+  }
+}
