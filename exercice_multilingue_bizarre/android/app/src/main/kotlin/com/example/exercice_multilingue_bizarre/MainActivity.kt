547a00bcd28d1799ba965ac881e8667388bab6db
Gbecet Kouame Djahakpl <2164425@cegepmontpetit.ca>
2024-06-17 11:40:42 -0400

    FCT: Multilangue

---
+++ exercice_multilingue_bizarre/android/app/src/main/kotlin/com/example/exercice_multilingue_bizarre/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exercice_multilingue_bizarre
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ exercice_multilingue_bizarre/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.of(context).title),
+      ),
+      body: Center(
+        child: Text(S.of(context).helloWorld),
+      ),
+    );
+  }
+}
